99f51a3ead89e72d0f069f658152e0282266e6fd
marquigi <markluigi3@yahoo.it>
2025-11-05 19:02:26 +0100

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+// NOTA
+import italiano from '@angular/common/locales/it';
+// NOTA
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(italiano);
+// NOTA
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+
+    { provide: LOCALE_ID, useValue: 'it-IT' }
+    // NOTA
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
+import { CurrencyPipe, DatePipe, DecimalPipe, KeyValuePipe, TitleCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe, TitleCasePipe, CurrencyPipe, DecimalPipe, KeyValuePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pipes');
+  oggi = new Date();
+  // NOTA
+
+  // NOTA
+  genericObj: { [key: string]: string } = {
+    CPU: "Intel Core i7",
+    RAM: "16GB",
+    HDD: "2TB"
+  }
 }
